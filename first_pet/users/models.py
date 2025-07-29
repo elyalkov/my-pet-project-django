b61bf6db1311e94860dda56c9d95f17bda7f40db
e.Lyalkov <e_lyalkov@mail.ru>
2025-07-29 11:12:52 +0300

    Написал пояснения к users/models

--- first_pet/users/models.py
+++ first_pet/users/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+#AbstractBaseUser — базовый класс для создания своей кастомной модели пользователя (без username)
+#BaseUserManager — менеджер, который управляет созданием пользователей
+#PermissionsMixin — добавляет поддержку прав (например, is_superuser, groups, permissions)
 
 
 class UserManager(BaseUserManager):
-    def creeate_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
+                   #self — ссылка на экземпляр UserManager (менеджер пользователей)
+                   #email — обязательный аргумент, по которому пользователь будет идентифицироваться
+                   #password — необязательный аргумент; если не передан — будет None
+                   #**extra_fields — дополнительные поля (например, имя, город и т.д.)
         if not email:
             raise ValueError('The email field must be set')
         email = self.normalize_email(email)
+                #Метод normalize_email из BaseUserManager риводит email к "нормальному" виду (например, Test@Email.com → test@email.com)
         user = self.model(email=email, **extra_fields)
+                #Создание экземпляра модели пользователя, self.model — ссылка на кастомную модель User, с которой связан UserManager
         user.set_password(password)
+                #Метод set_password хэширует пароль
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        #Django требует, чтобы суперпользователь имел доступ к админке и все права
+        extra_fields.setdefault('is_staff', True) #доступ к админке
+        extra_fields.setdefault('is_superuser', True) #все права
 
-        return self.creeate_user(email, password, **extra_fields)
+        return self.creeate_user(email, password, **extra_fields) #используем функцию для юзера, но с параметрами супера
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -26,15 +36,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30, blank=True)
     middle_name = models.CharField(max_length=30, blank=True)
     city = models.CharField(max_length=30, blank=True)
-    streer = models.CharField(max_length=30, blank=True)
+    street = models.CharField(max_length=30, blank=True)
     house_number = models.CharField(max_length=10, blank=True)
     apartment_number = models.CharField(max_length=10, blank=True)
     postal_code = models.CharField(max_length=10, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True) #служебное поля для входа в систему
+    is_staff = models.BooleanField(default=False) #доступ к Django-админке
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email' #Django по умолчанию логинит по username (используй email вместо username)
+    REQUIRED_FIELDS = [] #список обязательных полей при создании суперпользователя (email уже обязателен по умолчанию)
 
 
     def __str__(self):
