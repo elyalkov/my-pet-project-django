78f6c36451d5dc838a1d64e4818fd201d066351c
e.Lyalkov <e_lyalkov@mail.ru>
2025-08-29 11:10:21 +0300

    Добавил поиск питомцев

--- first_pet/main/views.py
+++ first_pet/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.db.models import Q
 
 
 def product_list(request, category_slug=None): #ategory_slug - параметр запроса при фильтрации
@@ -12,10 +13,15 @@ def product_list(request, category_slug=None): #ategory_slug - параметр 
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
     return render(request, 'main/product_list.html',
                   {'category': category,
                    'categories': categories,
-                   'products': products})
+                   'products': products,
+                   'query': query})
 
 
 def product_detail(request, slug):
