d21bcaa1a92f9086c47156657985095022d9f62c
e.lyalkov <e.lyalkov@gmail.com>
2025-09-12 20:12:25 +0300

    Перевел на русский forms.py в orders и users

--- first_pet/orders/forms.py
+++ first_pet/orders/forms.py
@@ -7,3 +7,12 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'phone_number',
                   'city', 'street', 'house_number', 'apartment_number']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'phone_number': 'Телефон',
+            'city': 'Город',
+            'street': 'Улица',
+            'house_number': 'Номер дома',
+            'apartment_number': 'Номер квартиры',
+        }
--- first_pet/users/forms.py
+++ first_pet/users/forms.py
@@ -44,4 +44,14 @@ class UserProfileForm(forms.ModelForm):
         fields = [
             'first_name', 'last_name', 'phone_number',
             'city', 'street', 'house_number',
-            'apartment_number',]
+            'apartment_number',
+        ]
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'phone_number': 'Телефон',
+            'city': 'Город',
+            'street': 'Улица',
+            'house_number': 'Номер дома',
+            'apartment_number': 'Номер квартиры',
+        }
