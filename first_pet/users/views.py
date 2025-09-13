56239d71e1a6d54a313f16ee6f13d67dadfcf277
e.lyalkov <e.lyalkov@gmail.com>
2025-09-13 14:08:27 +0300

    Доработал users

--- first_pet/users/views.py
+++ first_pet/users/views.py
@@ -12,7 +12,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('users:user_login')
+            return redirect('cart:cart_detail')
     else:
         form = UserRegistrationForm()
 
